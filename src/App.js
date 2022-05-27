530931e5831002bbcbe1ab0a9248aa116873cb2c
SaboTak <guideon.slayer@gmail.com>
2022-05-27 01:13:43 -0500

    Version Estable, falta lista de agrupacion y afectacion de la barra

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AiOutlineCheck } from "react-icons/ai";
+
 
 function App() {
 
   var IngresosTotales = [];
+  var valorAnteriorIngresos = [];
   var EgresosTotales = [];
-
+  var valorAnteriorEgresos = [];
+  
   const [Ingreso, setIngreso] = useState();
   const [Egreso, setEgreso] = useState();
+  const [itemBarra, setItemBarra] = useState();
+  const [itemIngreso, setItemIngreso] = useState();
+  const [itemEgreso, setItemEgreso] = useState();
 
   const addIngreso = (event) => {
     event.preventDefault();
@@ -16,44 +23,98 @@ function App() {
       const Valor= event.target[1].value;
       const PeriodoReal= event.target[2].value;
       IngresosTotales.push({Ubicacion,Valor,PeriodoReal});
+      ListaIngresos();
     console.log(IngresosTotales,"Agregado")
   }
 
   const addEgreso = (event) => {
     event.preventDefault();
     const Ubicacion= event.target[0].value;
-      const Valor= event.target[1].value;
-      const PeriodoReal= event.target[2].value;
-      EgresosTotales.push({Ubicacion,Valor,PeriodoReal});
+    const Valor= event.target[1].value;
+    const PeriodoReal= event.target[2].value;
+    
+    EgresosTotales.push({Ubicacion,Valor,PeriodoReal});
+    ListaEgresos();
     console.log(EgresosTotales, "Agregado")
   }
 
+
+  const addCiclos = (e) => {
+    e.preventDefault();
+    var barraCiclos = e.target[0].value;
+    var itemsBarra = [];
+    for(var i = 0; i<=barraCiclos; i++){
+      itemsBarra.push(
+              <li id={"Ciclo-" +i} className="float-left	" >
+                    <div className="text-center -mt-16  -ml-20">
+                      <p className="text-2xl 	text-green-600 text-white ">🠕</p>
+                      <p>1</p>
+                      <button className="bg-black rounded-xl w-6 h-6	"></button>
+                      <p>{i}</p>
+                      <p className="text-2xl text-red-800	text-white ">🠗</p>
+                    </div>
+               </li>
+      )
+    }
+    setItemBarra(itemsBarra);
+  }
   
   const ListaIngresos = () => {
-    return <ul>
-            {IngresosTotales.map((item) => {
-              return(
-                <li key={item.Ubicacion} className=" col-span-3 text-center  grid grid-cols-4 gap-2 py-2">
-                    <label> {item.Ubicacion} </label>
-                    <label> {item.Valor} </label>
-                    <label> {item.PeriodoReal} </label>
-                    <button  type="submit" className="btn bg-green-600 text-white rounded-full  w-7  h-7  text-center text-center mx-16 ">
-                        X
-                    </button>
-              </li>)
-            })}
-          </ul>
+    var itemIngreso = [];
+    for(var j = 0; j<IngresosTotales.length; j++){
+      itemIngreso.push(
+          <li key={IngresosTotales[j].Ubicacion} className=" col-span-3 text-center  grid grid-cols-4 gap-2 py-2">
+                <label> {IngresosTotales[j].Ubicacion} </label>
+                <label> {IngresosTotales[j].Valor} </label>
+                <label> {IngresosTotales[j].PeriodoReal} </label>
+                <button  type="submit" className="btn bg-green-600 text-white rounded-full  w-7  h-7  text-center text-center mx-10 ">
+                    X
+                </button>
+          </li>
+    )
+    }
+    setItemIngreso(itemIngreso);
   }
 
-  useEffect(() => {
-    ListaIngresos();
-}, [IngresosTotales]);
+  const ListaEgresos = () => {
+    var itemEgreso = [];
+    for(var j = 0; j<EgresosTotales.length; j++){
+      itemEgreso.push(
+          <li key={EgresosTotales[j].Ubicacion} className=" col-span-3 text-center  grid grid-cols-4 gap-2 py-2">
+                <label> {EgresosTotales[j].Ubicacion} </label>
+                <label> {EgresosTotales[j].Valor} </label>
+                <label> {EgresosTotales[j].PeriodoReal} </label>
+                <button  type="submit" className="btn bg-green-600 text-white rounded-full  w-7  h-7  text-center text-center mx-10 ">
+                    X
+                </button>
+          </li>
+    )
+    }
+    setItemEgreso(itemEgreso);
+  }
+//   useEffect(() => {
+//     ListaIngresos();
+// }, [IngresosTotales]);
+
+
+
   return (
     <div className="">
       <header className="text-center bg-black text-white">
         <h1>Flujo de Caja</h1>
       </header>
 
+        {/*   Cantidad de Ciclos   */}
+
+          <form onSubmit={(e) => addCiclos(e)} className=" text-center ml-50	mt-4">
+                  <input className="text-black border-2 h-7   w-50 rounded-lg w-30" placeholder="Cantidad de Ciclos" type="number" name="Ciclos"  />
+                  <button type="submit" className="btn bg-green-600 text-white rounded-full  w-6 px-1  h-6 mt-2  ml-4 text-center ">
+                                <AiOutlineCheck />
+                  </button>
+              </form>
+
+        {/*   //////////////////   */}
+      
       {/*   Formularios   */}
       <div className="grid grid-cols-2 gap-2 ml-4 mr-4 pt-4"> 
               <div className="border rounded-md p-2">
@@ -61,7 +122,9 @@ function App() {
                       INGRESOS
                   </h1>
                   <div>
-                        {ListaIngresos}
+                        <ul >
+                          {itemIngreso}
+                        </ul>
                   </div>
                   <div className="p-2">
                       <form onSubmit={addIngreso} className=" grid grid-cols-4 p-x-2 gap-4">
@@ -84,16 +147,9 @@ function App() {
                   </h1>
                   <div>
                       <ul>
-                      {EgresosTotales.map((item) => {
-                        return <li key={item.Ubicacion} className=" col-span-3 text-center  grid grid-cols-4 gap-2 py-2">
-                                <label> {item.Ubicacion} </label>
-                                <label> {item.Valor} </label>
-                                <label> {item.PeriodoReal} </label>
-                                <button type="submit" className="btn bg-red-600 text-white rounded-full  w-7  h-7  text-center text-center mx-16 ">
-                                    X
-                                </button>
-                              </li>
-                      })}
+                      <ul >
+                          {itemEgreso}
+                        </ul>
 
                       </ul>
                   </div>
@@ -119,24 +175,7 @@ function App() {
           {/* /////Barra///// */}
           <div className=" mx-10 mt-40">
              <ul id="Barra">
-               <li className="float-left	" >
-                  <div className="text-center -mt-16  -ml-20">
-                    <p className="text-2xl 	text-green-600">🠕</p>
-                    <p>1</p>
-                    <button className="bg-black rounded-xl w-6 h-6	"></button>
-                    <p>1</p>
-                    <p className="text-2xl text-red-800	">🠗</p>
-                 </div>
-               </li>
-               <li className="float-left" >
-                  <div className="text-center -mt-16 -ml-20">
-                    <p className="text-2xl 	text-green-600">🠕</p>
-                    <p>2</p>
-                    <button className="bg-black rounded-xl w-6 h-6	"></button>
-                    <p>2</p>
-                    <p className="text-2xl text-red-800	">🠗</p>
-                 </div>
-               </li>
+               {itemBarra}
              </ul>
           </div>   
           {/* /////////////// */}
